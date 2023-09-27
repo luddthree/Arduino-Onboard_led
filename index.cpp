73b3f6fadb49d8359b252dc64a8a68ad352749cb
ludd3 <92044167+luddthree@users.noreply.github.com>
2023-09-27 14:52:36 +0200

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -1,24 +1,38 @@
 #define ONBOARD_LED 2
 
+const int bootButtonPin = 0;
+bool buttonPressed = false;
+
+
 void setup() {
   pinMode(ONBOARD_LED, OUTPUT);
-  Serial.begin(115200); // Open the serial port at 115200 bps
+  Serial.begin(115200); 
   Serial.write("connected");
-  
+  pinMode(bootButtonPin, INPUT_PULLUP); 
 }
 
 void loop() {
+  if (digitalRead(bootButtonPin) == LOW) {
+    if (!buttonPressed) {
+      digitalWrite(ONBOARD_LED, HIGH);
+      buttonPressed = true; 
+    }
+  } else {
+    buttonPressed = false; 
+    digitalWrite(ONBOARD_LED, LOW); 
+  }
+
+  
   if (Serial.available()) {
-    char receivedChar = Serial.read(); // Read the received character
+    char receivedChar = Serial.read(); 
 
     if (receivedChar == '1') {
-      digitalWrite(ONBOARD_LED, HIGH); // Turn the LED on
+      digitalWrite(ONBOARD_LED, HIGH); 
       Serial.write(receivedChar);
       delay(1000);
     } else {
-      digitalWrite(ONBOARD_LED, LOW); // Turn the LED off
+      digitalWrite(ONBOARD_LED, LOW); 
       Serial.write(receivedChar);
     }
   }
 }
-
